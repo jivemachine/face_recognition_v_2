6987e3c61bcbf7466bbf6344e4178eedba0c2970
jivemachine <the.jeremy.cobb@gmail.com>
2021-01-04 12:10:43 -0600

    working on prepping data function

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,3 +1,5 @@
+# run script in command line: python facial_recognition.py
+
 # imports
 import cv2
 import os
@@ -22,3 +24,27 @@ def detect_face(img):
 
     # return only the face
     return gray[y:y+w, x:x+h], faces[0]
+
+# this function reads all persons' training images, detects their face from each image
+# and returns two lists of the same size
+def prepare_training_data(data_folder_path):
+    # -- STEP 1 --
+    # get directories
+    dirs = os.listdir(data_folder_path)
+    faces = []
+    labels = []
+
+    for dir_name in dirs:
+        # our subject directories start with the letter 's' so ignore any non-relevant directories if any
+        if dir_name.startswith('s'):
+            continue
+    
+    # -- Step 2 --
+    # extract label numbdr of subject from dir_name, format of dir_name = slabel, so removing letter 's'
+    # from dir_name will give us label
+    label = int(dir_name.replace("S", ""))
+
+    # build path of directory containing images for current subject directory
+    
+
+    
